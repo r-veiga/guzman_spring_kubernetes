fc8a183e85ccf0bee24f54c8d71423c40fa79059
r-veiga <roberto.veigaborneo@nielseniq.com>
2022-10-21 11:36:28 +0200

    Modify POM.XML files in parent and child

---
+++ msvc-usuarios/src/test/java/org/rveiga/springcloud/msvc/usuario/msvcusuarios/MsvcUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rveiga.springcloud.msvc.usuario.msvcusuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
